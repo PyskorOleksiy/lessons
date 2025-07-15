841222891311f6c7ac0ccd5cb2cef019616d30c6
PyskorOleksiy <76560253+PyskorOleksiy@users.noreply.github.com>
2025-07-15 13:50:22 +0300

    Delete lesson_24 directory

--- lesson_24/js/global.js
+++
@@ -1,4 +0,0 @@
-export const header = document.querySelector(".header")
-export const footer = document.querySelector(".footer")
-export const counterItems = document.querySelectorAll('.counter .item--standart')
-export const observerCounterItems = document.querySelectorAll('.counter .item--observer')
